0645c849e1fcbca11908e2e0b711ddf907d6bfb6
Morthine <xswdyj@163.com>
2019-01-18 14:45:17 +0800

    day3

--- src/components/news/NewList.vue
+++ src/components/news/NewList.vue
@@ -3,7 +3,49 @@
   <div>
 
 
-    <h1>12222</h1>
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="../../images/shuijiao.jpg">
+          <div class="mui-media-body">
+            幸福
+            <p class="mui-ellipsis">
+              <span>发表时间：2019-01-18</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>点击：10次</span>
+            </p>
+
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="../../images/muwu.jpg">
+          <div class="mui-media-body">
+            木屋
+            <p class="mui-ellipsis">
+
+              <span>发表时间：2019-01-18</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>点击：10次</span>
+            </p>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="../../images/cbd.jpg">
+          <div class="mui-media-body">
+            CBD
+            <p class="mui-ellipsis">
+              <span>发表时间：2019-01-18</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>点击：10次</span>
+            </p>
+          </div>
+        </a>
+      </li>
+
+    </ul>
+
+
   </div>
 
 </template>
@@ -14,6 +56,23 @@
   }
 </script>
 
-<style scoped>
+<style  scoped>
+
+  .mui-table-view div{
+    font-size: 14px;
+
+    }
+
+  .mui-table-view .mui-ellipsis{
+
+    font-size: 12px;
+    color: #007aff;
+    display: flex;
+    justify-content: space-between;
+
+  }
+
+
+
 
 </style>
--- src/router.js
+++ src/router.js
@@ -8,13 +8,17 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer'
 import VipContainer from './components/tabbar/VipContainer'
 
 
+import NewList from './components/news/NewList'
+
+
 const router = new VueRouter({
   routes: [
     {path: '/' ,component: HomeContainer},
     {path: '/search' ,component: SearchContainer},
     {path: '/home' ,component: HomeContainer},
     {path: '/vip' ,component: VipContainer},
-    {path: '/shopcar' ,component: ShopcarContainer}
+    {path: '/shopcar' ,component: ShopcarContainer},
+    {path:'/home/newslist',component: NewList}
 
   ],
 
